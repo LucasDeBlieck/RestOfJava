6b00dfa5eef0520221cc41718475ab6c3a216f65
s-ldeblieck <s-ldeblieck@484E293874.lwsd.k12wa.org>
2022-06-06 14:33:51 -0700

    3rd

--- UserInterfaceDemo/src/Program.java
+++ UserInterfaceDemo/src/Program.java
@@ -1,8 +1,13 @@
+import java.awt.Frame;
 
 public class Program {
 	
 	public static void Main(String[] args) {
 		System.out.println("Hello Java UI Demo");
+		
+		Frame myFrame = new Frame("Java UI Demo");
+		myFrame.setSize(800, 600);
+		myFrame.setVisible(true);
 		
 		System.out.println("Goodbye!");
 	}
