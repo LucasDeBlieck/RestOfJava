55d8f0e1c2ac42a262c397d56296b315086e773c
s-ldeblieck <s-ldeblieck@484E293874.lwsd.k12wa.org>
2022-06-09 12:00:50 -0700

    better

--- UserInterfaceDemo/src/MyListener.java
+++ UserInterfaceDemo/src/MyListener.java
@@ -1,47 +1,72 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.TextArea;
 
-public class MyListener implements WindowListener {
-
+public class MyListener implements WindowListener, ActionListener {
+	private TextArea textarea;
+	
+	public MyListener(TextArea textarea) {
+		this.textarea = textarea;
+	}
+	
+	private void printToText(String text) {
+String currentText = textarea.getText();
+String newText = currentText + "\n" + text;
+textarea.setText(newText);
+textarea.setCaretPosition(newText.length());
+}
+	
 	@Override
 	public void windowActivated(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		printToText("windowActivated");
 		
 	}
 
 	@Override
 	public void windowClosed(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		printToText("windowClosed");
 		
 	}
 
 	@Override
 	public void windowClosing(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		printToText("windowClosing");
+		((Frame)arg0.getSource()).dispose();
 		
 	}
 
 	@Override
 	public void windowDeactivated(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		printToText("windowDeactivated");
 		
 	}
 
 	@Override
 	public void windowDeiconified(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		printToText("windowDeiconified");
 		
 	}
 
 	@Override
 	public void windowIconified(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		printToText("windowIconified");
 		
 	}
 
 	@Override
 	public void windowOpened(WindowEvent arg0) {
-		// TODO Auto-generated method stub
+		printToText("windowOpened");
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Button button = (Button)arg0.getSource();
+		printToText(button.getLabel());
 		
 	}
 
--- UserInterfaceDemo/src/Program.java
+++ UserInterfaceDemo/src/Program.java
@@ -1,14 +1,48 @@
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Frame;
+import java.awt.Insets;
+import java.awt.TextArea;
 
 public class Program {
-	
-	public static void Main(String[] args) {
+
+private static 	Frame myFrame = new Frame("Java UI Demo");	
+	private static final int _padding = 10;
+	private static Button myButton = new Button("My First Button!");
+	private static TextArea myTextArea = new TextArea();
+	private static MyListener myListener = new MyListener(myTextArea);
+
+	public static void main(String[] args) {
 		System.out.println("Hello Java UI Demo");
-		
-		Frame myFrame = new Frame("Java UI Demo");
+
+	
 		myFrame.setSize(800, 600);
 		myFrame.setVisible(true);
+
 		
+		myFrame.addWindowListener(myListener);
+		myFrame.setLayout(null);
+
+		Dimension d = myFrame.getSize();
+		System.out.println(d);
+		Insets i = myFrame.getInsets();
+		System.out.println(i);
+
+		
+		myButton.setBounds(i.left + _padding, i.top + _padding, 100, 40);
+
+		myFrame.add(myButton);
+		myButton.setBackground(Color.cyan);
+		myButton.addActionListener(myListener);
+
+		
+		int xAnchor = myButton.getX();
+		int yAnchor = myButton.getY() + myButton.getHeight() + _padding;
+		myTextArea.setBounds(xAnchor, yAnchor, myFrame.getWidth() - i.left - i.right - 2 * _padding,
+				myFrame.getHeight() - yAnchor -i.bottom - _padding);
+		myFrame.add(myTextArea);
 		System.out.println("Goodbye!");
+		myTextArea.setBackground(Color.gray);
 	}
 }
